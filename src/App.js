63e04662122206ab5863e4561e5802ae17435842
KhanhLinh20 <khanhlinhdh.work@gmail.com>
2025-06-01 13:54:29 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Welcome/>
     </>
   );
 }
