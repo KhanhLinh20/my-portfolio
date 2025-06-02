de28c778942e748d57f2271baaba5ede6e40126f
KhanhLinh20 <khanhlinhdh.work@gmail.com>
2025-06-02 20:46:58 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar />
       <Welcome/>
+      <About/>
     </>
   );
 }
